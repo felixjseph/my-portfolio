6e2700cc0894eabcc138f9a5951b4c30e1069220
Felix Castañeda <felixjoseph.castaneda@cit.edu>
2025-12-02 00:27:41 +0800

    Final Design
    FInal Navbar & Landing Page design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Mail, Download} from "lucide-react";
+import {Mail, Download, Github, Linkedin} from "lucide-react";
 
 export default function Home() {
     return (
-        <section id="home" className='min-h-screen grid place-items-center bg-gradient-to-b from-white to-slate-50'>
+        <section id="home" className='min-h-screen grid place-items-center bg-linear-to-b from-white via-blue-50/40 to-purple-200'>
             <div className='max-w-4xl text-center px-6 py-20'>
                 <p className='text-xl'>Hi, I'm</p>
-                <h1 className='text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent mb-4'>Felix Joseph U. Castaneda</h1>
+                <h1 className='text-6xl md:text-6xl font-extrabold bg-linear-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent mb-4'>Felix Joseph U. Castaneda</h1>
                 <p className='text-xl md:text-xl text-slate-600 mb-6 font-medium'>Aspiring IT Professional | BSIT Student</p>
-                <p className="text-lg mb-6 text-slate-600">
+                <p className="text-xl mb-12 text-slate-600">
                     Building Modern Web Solutions Through Innovation & Learning.
                     4th year BSIT student passionate about creating innovative web applications using modern technologies.
                 </p>
@@ -17,23 +17,46 @@ export default function Home() {
                
                     <a
                         href="#contact"
-                        className="inline-flex items-center justify-center px-6 py-3 bg-gradient-to-r from-blue-400 to-purple-600 text-white rounded-md font-medium transition-all duration-300 ease-in-out transform hover:shadow-xl hover:-translate-y-1 group"
+                        className="inline-flex items-center justify-center px-6 py-3 bg-linear-to-r from-blue-400 to-purple-600 text-white rounded-md font-medium transition-all duration-300 ease-in-out transform hover:shadow-xl hover:-translate-y-1 group"
                         >
                         <Mail className="w-5 h-5 mr-2 transition-all duration-300 ease-in-out group-hover:-translate-y-0.5" />
                         Get in touch
                     </a>
 
-
-             
                      <a
                         href="/resume.pdf"
                         download
-                        className="inline-flex items-center justify-center px-6 py-3 border border-slate-200 rounded-md font-medium text-gray-800 transition-all duration-300 group hover:bg-white hover:border-gray-400"
+                        className="inline-flex items-center justify-center px-6 py-3 border border-slate-300 rounded-md font-medium text-gray-800 transition-all duration-300 group hover:bg-white hover:border-gray-200"
                     >
                         <Download className="w-5 h-5 mr-2 transition-transform duration-400 group-hover:animate-bounce" />
                         Download Resume
                     </a>
                     </div>
+
+                    <div className="flex justify-center gap-6 mt-14">
+                    <a 
+                        href="https://github.com/felixjseph"
+                        target="_blank" 
+                        className="text-blue-600 hover:scale-110 transition-colors"
+                    >
+                        <Github className="w-7 h-7" />
+                    </a>
+
+                    <a 
+                        href="https://www.linkedin.com/in/felixjseph/" 
+                        target="_blank"
+                        className="text-blue-600 hover:scale-110 transition-colors"
+                    >
+                        <Linkedin className="w-7 h-7" />
+                    </a>
+
+                    <a 
+                        href=""
+                        className="text-blue-600  hover:scale-110 transition-colors"
+                    >
+                        <Mail className="w-7 h-7" />
+                    </a>
+                </div>
 
             </div>
         </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import React, {useState} from "react";
 import {Menu, X} from "lucide-react";
 
 const NavLink = ({ href, children }) => (
-  <a href={href} className="text-gray-800 hover:text-blue-600 transition-colors font-medium">
+  <a 
+    href={href} 
+    className="relative text-gray-800 hover:text-blue-600 transition-colors font-medium 
+    before:absolute before:-bottom-0.5 before:left-0 before:h-0.5 before:w-0 before:bg-blue-600 
+    before:transition-all before:duration-300 hover:before:w-full"
+  >
     {children}
   </a>
 )
@@ -13,7 +18,12 @@ export default function Navbar() {
     return (
       <header className="sticky top-0 bg-gray-50 backdrop-blur z-30 shadow-sm">
         <div className="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
-          <a href="#home" className="text-3xl font-bold bg-gradient-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent cursor-pointer">FJC</a>
+          <a 
+           href="#home" 
+            className="text-3xl font-bold bg-linear-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent cursor-pointer"
+            > 
+             FJC
+          </a>
 
         <nav className="space-x-8 hidden md:flex">
           <NavLink href="#home">Home</NavLink>
@@ -24,32 +34,34 @@ export default function Navbar() {
           <NavLink href="#contact">Contact</NavLink>
         </nav>
 
-        <div className="md:hidden flex items-center">
+        <div className="md:hidden">
           <button
             onClick={() => setIsOpen (!isOpen)}
-            className="text-gray-800 focus:outline-none hover hover:text-blue-600 cursor-pointer"
+            className="focus:outline-none text-blue-600 cursor-pointer"
           >
             {isOpen ? <X size={28} /> : <Menu size={28} />}
             </button>
           </div>
         </div>
 
-        {isOpen && (
-          <nav className="md:hidden bg-white shadow-lg">
-            <div className="flex flex-col space-y-3 px-6 py-4">
-               {["Home","About","Services","Projects","Skills","Contact"].map((item) => (
-            <a
-                key={item}
-                href={`#${item.toLowerCase()}`}
-                className="px-4 py-2 rounded hover:bg-blue-100 transition-colors font-medium text-gray-800"
-              >
+        <nav className={`md:hidden absolute p-2 w-full bg-white shadow-lg transition-all duration-300 overflow-hidden ${
+          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+        >
+          <div className="flex flex-col space-y-3 px-6 py-4">
+              {["Home","About","Services","Projects","Skills","Contact"].map(
+              (item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="px-4 py-2 rounded hover:bg-blue-100 transition-colors font-medium text-gray-800"
+                  onClick={() => setIsOpen(false)}
+                >
                   {item}
                 </a>
                ))}
             </div>
           </nav>
-        )}
-
       </header>
   );
 }
