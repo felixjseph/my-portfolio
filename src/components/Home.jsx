68e3e2bbbff4f20ba55395d2ad75562bb482ee38
Felix Castañeda <felixjoseph.castaneda@cit.edu>
2025-12-02 01:12:42 +0800

    Mobile Fixes
    Mobile Design Fixes for Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ export default function Home() {
         <section id="home" className='min-h-screen grid place-items-center bg-linear-to-b from-white via-blue-50/40 to-purple-200'>
             <div className='max-w-4xl text-center px-6 py-20'>
                 <p className='text-xl'>Hi, I'm</p>
-                <h1 className='text-6xl md:text-6xl font-extrabold bg-linear-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent mb-4'>Felix Joseph U. Castaneda</h1>
-                <p className='text-xl md:text-xl text-slate-600 mb-6 font-medium'>Aspiring IT Professional | BSIT Student</p>
-                <p className="text-xl mb-12 text-slate-600">
+                <h1 className='text-3xl md:text-6xl font-extrabold bg-linear-to-r from-blue-400 to-purple-600 bg-clip-text text-transparent mb-4'>Felix Joseph U. Castaneda</h1>
+                <p className='text-lg md:text-xl text-slate-600 mb-6 font-medium'>Aspiring IT Professional | BSIT Student</p>
+                <p className="md:text-lg mb-12 text-slate-600">
                     Building Modern Web Solutions Through Innovation & Learning.
                     4th year BSIT student passionate about creating innovative web applications using modern technologies.
                 </p>
